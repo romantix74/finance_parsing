e125a9643535a87b865e4f271f17b8ff24824744
romantix74 <romantix74@list.ru>
2019-11-07 22:16:00 +0300

    trying example from inet

--- parse_detmir.py
+++ parse_detmir.py
@@ -1,7 +1,20 @@
 import os
 
+pdf_document = "input/9m-2019_IFRS_RUS.pdf"  
+
 def print_pdf():
   print("123")
+  from PyPDF2 import PdfFileReader
+  
+  with open(pdf_document, "rb") as filehandle:  
+   pdf = PdfFileReader(filehandle)
+   info = pdf.getDocumentInfo()
+   pages = pdf.getNumPages()   
+   print (info)
+   print ("number of pages: %i" % pages)   
+   page1 = pdf.getPage(0)
+   print(page1)
+   print(page1.extractText())
 
 if __name__ == "__main__":
   print_pdf()
