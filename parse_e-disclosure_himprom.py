de5e7d0ad534992f0e9af678b7e3fcc49ec64dca
romantix74 <romantix74@list.ru>
2019-12-15 22:18:13 +0300

    there is  usable text extractred

--- parse_e-disclosure_himprom.py
+++ parse_e-disclosure_himprom.py
@@ -27,19 +27,40 @@ import csv
       # print(link) # .get('href'))
       # #print(link.contents)
 
+elements_found = []
 
-def get_html(url):
+def get_html( url ):
     r = requests.get(url)    # Получим метод Response
     r.encoding = 'utf8'
     return r.text   # Вернем данные объекта text
 	
-def get_bsoup_data(html):
-  soup = BeautifulSoup(html, 'lxml')
-  #print(soup.findAll('a'))
+def get_bsoup_data( html ):
+  soup = BeautifulSoup( html, 'lxml' )
+
   for link in soup.find_all('a'):
     if ("Выплаченные доходы по эмиссионным ценным бумагам эмитента" in link.contents):  
-      print(link.get('href'))
-		
-data = get_bsoup_data(get_html(disclosure_himprom))
+      #print(link.get('href'))
+      elements_found.append(link.get('href'))
+	  
+  return elements_found
 
-print(data)
+def get_bsoup_in_report( html ):
+  elements_found = []  # обнуляем
+  soup = BeautifulSoup( html, 'lxml' )
+  	  
+  return soup.find('div', id='cont_wrap').text
+		
+if __name__ == '__main__':
+ 		
+  # сначала находим все ссылки на события выплат
+  data_links = get_bsoup_data(get_html(disclosure_himprom))
+
+  print(data_links)
+  
+  # скачиваем по полученным ссылкам
+  if data_links is not None:
+    for link in data_links:
+      print("---------------------------------------------------------------")
+      print(get_bsoup_in_report(get_html(link)))
+      
+  
